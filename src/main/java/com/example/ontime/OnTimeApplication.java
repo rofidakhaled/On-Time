acb6d2256d7705758e41ea22b71fb535c2de6fbb
R00V <s-rofida.moustafa@zewailcity.edu.eg>
2024-12-26 14:38:43 +0200

    init

---
+++ src/main/java/com/example/ontime/OnTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnTimeApplication.class, args);
+    }
+//hi
+}
